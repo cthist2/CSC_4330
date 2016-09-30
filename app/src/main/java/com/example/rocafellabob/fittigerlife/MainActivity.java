f9e714d830be2b48e81b94e9bcf2533d18f64f23
spencerjanin1 <fella.roca@yahoo.com>
2016-09-29 18:36:59 -0600

    Implemented Activity Change
    Implemented the first activity change. This change will make the edit
    goal button bring you to a new activity

--- app/src/main/java/com/example/rocafellabob/fittigerlife/MainActivity.java
+++ app/src/main/java/com/example/rocafellabob/fittigerlife/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.rocafellabob.fittigerlife;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void moveGoals(View view) {
+        Intent intent = new Intent(this,MoveGoalsActivity.class);
+        startActivity(intent);
+    }
+
 }
