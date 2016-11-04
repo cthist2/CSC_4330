9a14aef683ac05f9b13e3c7348ed3bf8ee3bfb43
spencerjanin1 <fella.roca@yahoo.com>
2016-11-03 19:40:27 -0600

    Built Frontend of cardio
    I have finshed the front in to cardio, if im not mistaken this should
    conclude building the UI. So I will start wroking on backend

--- app/src/main/java/com/example/rocafellabob/fittigerlife/CardioActivity.java
+++ app/src/main/java/com/example/rocafellabob/fittigerlife/CardioActivity.java
@@ -2,9 +2,10 @@ package com.example.rocafellabob.fittigerlife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class CardioActivity extends AppCompatActivity {
-
+    private Spinner spinner2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
